e0f25e0729eaad606975eda7a5855c84e5a11576
Apocryphon06 <hrithikchandrashekar@gmail.com>
2023-05-04 23:42:16 +0530

    updated list of projects_

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Footer from './components/Footer'
 import hexa from './assets/images/hexa.png'
 import Skills from './components/pages/Skills'
 import Separator from './components/Separator'
-import amazon from './assets/images/amazon.png'
+// import amazon from './assets/images/amazon.png'
 import Projects from './components/pages/Projects'
 import whatsapp from './assets/images/whatsapp.png'
 import linkedin from './assets/images/linkedin.png'
@@ -14,8 +14,24 @@ import zaincare from './assets/images/zaincare.png'
 import Portfolio from './components/pages/Portfolio'
 import Education from './components/pages/Education'
 import starbucks from './assets/images/starbucks.png'
+import inito from './assets/images/inito.png'
+
 
 const projects = [
+  {
+    id: 5,
+    title: 'zaincare',
+    description: 'web application',
+    link: 'https://zaincare.com',
+    img: zaincare,
+  },
+  {
+    id: 10,
+    title: 'inito',
+    description: 'website design',
+    link: 'https://inito.netlify.app',
+    img: inito,
+  },
   {
     id: 1,
     title: 'hexa',
@@ -27,7 +43,7 @@ const projects = [
     id: 2,
     title: 'linkedin clone',
     description: 'website redesign',
-    link: 'https://endearing-parfait-31c67f.netlify.app',
+    link: 'https://apocryphon06-linkedin-clone.netlify.app',
     img: linkedin,
   },
   {
@@ -37,13 +53,7 @@ const projects = [
     link: 'https://apocryphon06.github.io/totalitycorp-frontend-challenge/',
     img: starbucks,
   },
-  {
-    id: 5,
-    title: 'zaincare',
-    description: 'web application',
-    link: 'https://zaincare.scube.me',
-    img: zaincare,
-  },
+  
   {
     id: 8,
     title: 'whatsapp clone',
@@ -51,19 +61,20 @@ const projects = [
     link: 'https://apocryphon06-whatsapp-clone.netlify.app',
     img: whatsapp,
   },
-  {
-    id: 9,
-    title: 'amazon clone',
-    description: 'website design',
-    link: 'https://apocryphon06-amazon-clone.netlify.app',
-    img: amazon,
-  },
+  // {
+  //   id: 9,
+  //   title: 'amazon clone',
+  //   description: 'website design',
+  //   link: 'https://apocryphon06-amazon-clone.netlify.app',
+  //   img: amazon,
+  // },
+  
 ]
 
 function App() {
   return (
     <Fragment>
-      <div className="flex flex-col">
+      <div className="flex flex-col overflow-x-hidden">
         <Navbar />
         <Portfolio />
         <Separator />
--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,30 +1,28 @@
 import React from 'react'
 import Heading from '../Heading'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 
-const Projects = ({projects}) => {
+const Projects = ({ projects }) => {
   return (
     <div id="projects" className="px-10">
-          <Heading title="Projects" />
-
-          <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10 items-center mt-[100px]">
-            {projects.map((item) => (
-              <motion.div className="flex flex-col border p-5 rounded-sm hover:scale-[0.9] cursor-pointer transition-all">
-                <a href={item.link} target="__blank">
-                  <img
-                    src={item.img}
-                    alt={item.img}
-                    className="object-fit bg-[#dfdfdf] shadow-md rounded-sm"
-                  />
-                </a>
-                  <p className="uppercase tracking-widest font-sans lg:text-xl text-lg py-2 font-semibold">
-                    {item.title}
-                  </p>
-                  
-              </motion.div>
-            ))}
-          </div>
-        </div>
+      <Heading title="Projects" />
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10 items-center mt-[100px]">
+        {projects.map((item) => (
+          <motion.div className="flex flex-col border p-5 rounded-sm hover:scale-[0.9] cursor-pointer transition-all">
+            <a href={item.link} target="__blank">
+              <img
+                src={item.img}
+                alt={item.img}
+                className="object-fit bg-[#dfdfdf] shadow-md rounded-sm"
+              />
+            </a>
+            <p className="uppercase tracking-widest font-sans lg:text-xl text-lg py-2 font-semibold">
+              {item.title}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </div>
   )
 }
 
